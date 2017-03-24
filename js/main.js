d6f1ce3e68aeeb7560bb0582a96d2e9b909c9bf5
nathabonfim59 <nathabonfim59@gmail.com>
2017-03-24 18:05:02 -0300

    :space_invader: Fixed Mobile Navbar
    :space_invader: Fixed collapse menu size

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-$(document).ready(function () {});
+function checkIfIsMobile (windowWidth) {
+    if(windowWidth <= 480) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+var isMobile;
+
+$(document).ready(function () {
+    $(window).resize(function(){
+        isMobile = checkIfIsMobile($(window).width());
+        if(isMobile) {
+            $("#logo-text").text($(window).width());
+        }
+    });
+    
+});
